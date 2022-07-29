2161a9a2c439abf68a32e120f627a703af248c68
rich <adukyerer@gmail.com>
2022-07-29 09:05:14 +0000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Provider } from 'react-redux';
+import TestRenderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import React from 'react';
+import store from './redux/configureStore';
+import Mainpage from './components/Mainpage';
+import Country from './components/Country';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders Mainpage', () => {
+  render(
+    <Provider store={store}>
+      <Mainpage />
+    </Provider>,
+  );
+  const linkElement = screen.getByText(/Stats By Countries/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it('Country Page renders correctly', () => {
+  const Tree = TestRenderer.create(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Country />
+      </BrowserRouter>
+    </Provider>
+    ,
+  );
+  expect(Tree).toMatchSnapshot();
+});
+
+it('Main page renders correctly', () => {
+  const Tree = TestRenderer.create(
+    <Provider store={store}>
+      <Mainpage />
+    </Provider>,
+  );
+  expect(Tree).toMatchSnapshot();
 });
